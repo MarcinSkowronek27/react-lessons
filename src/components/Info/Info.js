c3c45993fb3033b0ba9498c891f95dbb8404fb4d
Marcin Skowronek <marcinfoto35@op.pl>
2021-07-09 15:43:25 +0200

    Prepared info with dataStore connection

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,18 +2,13 @@ import React from 'react';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
+import { infoData } from '../../data/dataStore';
 
 const Info = () => (
   <Container>
-    <Hero titleText='Info' image={'https://images.unsplash.com/photo-1625682734260-40d190fb4648?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'} />
+    <Hero titleText={infoData.title} image={infoData.image} />
     {/* <h2>Info</h2> */}
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-      dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>{infoData.text}</p>
   </Container>
 );
 
