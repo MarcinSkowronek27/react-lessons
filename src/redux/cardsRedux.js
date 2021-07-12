9286961a07e6067c458681c8c689d61f62ef5846
Marcin Skowronek <marcinfoto35@op.pl>
2021-07-13 00:02:52 +0200

    Cleaned headquarters in cardRedux

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumn = ({cards, searchString}, columnId) => cards.filter(card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title));
+export const getCardsForColumn = ({ cards }, columnId) => cards.filter(card => card.columnId == columnId);
 export const getCards = ({ cards }, inputString) => cards.filter(card => new RegExp(inputString, 'i').test(card.title));
 // action name creator
 const reducerName = 'cards';
