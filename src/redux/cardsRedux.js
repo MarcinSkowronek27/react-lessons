7fbfc845d53e3a0204e9c63b6c31e89852a60ce1
Marcin Skowronek <marcinfoto35@op.pl>
2021-07-12 23:52:02 +0200

    Done task - working app

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,24 +1,24 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumn = ({ cards }, columnId) => cards.filter(card => card.columnId == columnId && new RegExp('i').test(card.title));
-
+export const getCardsForColumn = ({cards, searchString}, columnId) => cards.filter(card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title));
+export const getCards = ({ cards }, inputString) => cards.filter(card => new RegExp(inputString, 'i').test(card.title));
 // action name creator
 const reducerName = 'cards';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
-export const ADD_CARDS = createActionName('ADD_CARDS');
+export const ADD_CARD = createActionName('ADD_CARD');
 
 // action creators
-export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARDS });
+export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
 
 // reducer
-export default function reducer(statePart = [], action = {}) {
+export default function reducer(state = [], action = {}) {
   switch (action.type) {
-    case ADD_CARDS:
-      return [...statePart, action.payload];
+    case ADD_CARD:
+      return [...state, action.payload];
     default:
-      return statePart;
+      return state;
   }
 }
