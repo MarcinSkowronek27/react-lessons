66e8d4cb231d92b427916ce8b29ef351c183c198
Marcin Skowronek <marcinfoto35@op.pl>
2021-07-13 00:19:33 +0200

    Added fixed icon and title - not working

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,24 +3,31 @@ import styles from './SearchResults.scss';
 import PropTypes from 'prop-types';
 // import {settings} from '../../data/dataStore';
 import Card from '../Card/Card.js';
+import Icon from '../Icon/Icon';
 
 class SearchResults extends React.Component {
-    static propTypes = {
-      cards: PropTypes.array,
-      changeSearchString: PropTypes.func,
-      match: PropTypes.any,
-    }
-    
-    render() {
-      const { cards } = this.props;
-      return (
-        <section className={styles.component}>
-          {cards.map(cardData => (
-            <Card key={cardData.id} {...cardData} />
-          ))}
-        </section>
-      );
-    }
+  static propTypes = {
+    cards: PropTypes.array,
+    // changeSearchString: PropTypes.func,
+    // match: PropTypes.any,
+  }
+
+  render() {
+    const { cards } = this.props;
+    return (
+      <section className={styles.component}>
+        <h3 className={styles.title}>
+          <span className={styles.icon}>
+            <Icon name='cat' />
+          </span>
+          TITLE
+        </h3>
+        {cards.map(cardData => (
+          <Card key={cardData.id} {...cardData} />
+        ))}
+      </section>
+    );
+  }
 }
 
 export default SearchResults;
