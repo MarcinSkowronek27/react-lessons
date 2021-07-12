dadb1c96f48e7208e128955c69aa119aa333edd4
Marcin Skowronek <marcinfoto35@op.pl>
2021-07-12 23:39:37 +0200

    Corrected search H

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,46 +2,69 @@ import React from 'react';
 import styles from './Search.scss';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
-import Icon from '../Icon/Icon';
 import { settings } from '../../data/dataStore';
-import { withRouter } from 'react-router';
+import Icon from '../Icon/Icon';
+import Container from '../Container/Container';
+import {withRouter} from 'react-router';
+
 
 class Search extends React.Component {
-
   static propTypes = {
-    history: PropTypes.node,
+    text: PropTypes.string,
+    searchString: PropTypes.string,
+    changeSearchString: PropTypes.func,
+    countVisible: PropTypes.number,
+    countAll: PropTypes.number,
+    history: PropTypes.any,
+  }
+
+  static defaultProps = {
+    text: settings.search.defaultText,
   }
 
   state = {
-    value: '',
-    visibleButtons: false,
+    value: this.props.searchString || '',
   }
 
-  handleChange(event) {
+  handleChange(event){
     this.setState({
       value: event.target.value,
       visibleButtons: event.target.value.length > 0,
     });
   }
 
-  handleOK() {
+  handleOK(){
+    // this.props.changeSearchString(this.state.value);
     this.props.history.push(`/search/${this.state.value}`);
   }
 
+  componentDidUpdate(prevProps){
+    if(this.props.searchString != prevProps.searchString){
+      this.setState({value: this.props.searchString});
+    }
+  }
+
   render() {
-    const { value } = this.state;
-    const { icon } = settings.search;
+    const {text, countVisible, countAll} = this.props;
+    const {value} = this.state;
+    const {icon} = settings.search;
     return (
-      <div className={styles.component}>
-        <input
-          type='text'
-          value={value}
-          onChange={event => this.handleChange(event)}
-        />
-        <div className={styles.buttons}>
-          <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
+      <Container>
+        <div className={styles.component}>
+          <input
+            type='text'
+            placeholder={text}
+            value={value}
+            onChange={event => this.handleChange(event)}
+          />
+          <div className={styles.buttons}>
+            <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
+          </div>
+          <div>
+            { countVisible == countAll ? '' : `${countVisible} / ${countAll}` }
+          </div>
         </div>
-      </div>
+      </Container>
     );
   }
 }
